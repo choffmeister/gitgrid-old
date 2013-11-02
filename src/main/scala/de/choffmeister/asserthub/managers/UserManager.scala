48395c886abed8044211284a3121700fccc22d62
Christian Hoffmeister <mail@choffmeister.de>
2013-11-02 23:49:51 +0100

    Add UserManager.authenticate method

--- src/main/scala/de/choffmeister/asserthub/managers/UserManager.scala
+++ src/main/scala/de/choffmeister/asserthub/managers/UserManager.scala
@@ -10,4 +10,26 @@ object UserManager {
    * Returns list of all users ordered by their IDs.
    */
   def allUsers: List[User] = inTransaction(from(Database.users)(u => select(u) orderBy(u.id asc)).toList)
+  
+  /**
+   * Returns the User object if credentials are valid or else None.
+   */
+  def authenticate(userName: String, password: String): Option[User] = {
+    // search for user with matching name
+    val user = singleOrNone(Database.users.where(u => u.userName === userName.toLowerCase()))
+    if (user.isDefined) {
+      // check if password matches
+      if (user.get.passwordHash == password) {
+        return user
+      }
+    }
+    
+    None
+  }
+  
+  private def singleOrNone[R](query: Query[R]): Option[R] = {
+    val cappedList = query.take(1).toList
+    
+    if (cappedList.length == 1) Some(cappedList(0)) else None
+  }
 }
--- src/test/scala/de/choffmeister/asserthub/managers/UserManagerTest.scala
+++ src/test/scala/de/choffmeister/asserthub/managers/UserManagerTest.scala
@@ -6,7 +6,7 @@ import org.squeryl.PrimitiveTypeMode._
 import de.choffmeister.asserthub._
 
 class UserManagerTest extends DatabaseAwareTest {
-  @Test def test() {
+  @Test def testAllUsers() {
     def createUser(i: Int) = new User(s"user${i}", s"user${i}@invalid.domain.tld", s"First${i}", s"Last${i}")
     
     transaction {
@@ -17,6 +17,26 @@ class UserManagerTest extends DatabaseAwareTest {
       val usersFromDb = UserManager.allUsers
     
       assertEquals(1 to 5, usersFromDb.map(_.id))
+    }
+  }
+  
+  @Test def testAuthenticate() {
+    transaction {
+      Database.drop
+      Database.create
+
+      val user1 = Database.users.insert(new User(0L, "user1", "mail1", "pass1", "", "", "", ""))
+      val user2 = Database.users.insert(new User(0L, "user2", "mail2", "pass2", "", "", "", ""))
+      
+      assertEquals(None, UserManager.authenticate("unknown", "pass"))
+      assertEquals(None, UserManager.authenticate("user1", "wrong"))
+      assertEquals(None, UserManager.authenticate("user2", "wrong"))
+      assertEquals(None, UserManager.authenticate("user1", "pass2"))
+      assertEquals(None, UserManager.authenticate("user2", "pass1"))
+      assertEquals(Some(user1), UserManager.authenticate("user1", "pass1"))
+      assertEquals(Some(user2), UserManager.authenticate("user2", "pass2"))
+      assertEquals(Some(user1), UserManager.authenticate("UsEr1", "pass1"))
+      assertEquals(Some(user2), UserManager.authenticate("uSeR2", "pass2"))
     }
   }
 }
