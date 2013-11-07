8137ce522b20361186c62d390fd8ab7aeb54e41d
Christian Hoffmeister <mail@choffmeister.de>
2013-11-07 22:33:38 +0100

    Change array style JSON format to object style

--- src/main/scala/de/choffmeister/asserthub/JsonProtocol.scala
+++ src/main/scala/de/choffmeister/asserthub/JsonProtocol.scala
@@ -6,13 +6,22 @@ import spray.json._
 object JsonProtocol extends DefaultJsonProtocol {
   implicit object UserJsonFormat extends RootJsonFormat[User] {
     def write(u: User) =
-      JsArray(JsNumber(u.id), JsString(u.userName), JsString(u.email), JsString(u.firstName), JsString(u.lastName))
+      JsObject(Map(
+        "id" -> JsNumber(u.id),
+        "userName" -> JsString(u.userName),
+        "email" -> JsString(u.email),
+        "firstName" -> JsString(u.firstName),
+        "lastName" -> JsString(u.lastName)
+      ))
+          
+      //JsArray(JsNumber(u.id), JsString(u.userName), JsString(u.email), JsString(u.firstName), JsString(u.lastName))
 
-    def read(value: JsValue) = value match {
-      case JsArray(JsNumber(id) :: JsString(userName) :: JsString(email) :: JsString(firstName) :: JsString(lastName) :: Nil) =>
-        new User(id.toLong, userName, email, firstName, lastName, "", "", "")
-        
-      case _ => deserializationError("User expected: " + value)
-    }
+    def read(value: JsValue) =
+      value.asJsObject.getFields("id", "userName", "email", "firstName", "lastName") match {
+        case Seq(JsNumber(id), JsString(userName), JsString(email), JsString(firstName), JsString(lastName)) =>
+          new User(id.toLong, userName, email, firstName, lastName, "", "", "")
+        case _ =>
+          deserializationError("User expected: " + value)
+      }
   }
 }
