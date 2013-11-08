b088328aa7c3a0e38d725fd75754435dbc76e240
Christian Hoffmeister <mail@choffmeister.de>
2013-11-08 14:47:09 +0100

    Enrich EntityRepository with implementations for CRUD operations

--- src/main/scala/de/choffmeister/asserthub/managers/UserManager.scala
+++ src/main/scala/de/choffmeister/asserthub/managers/UserManager.scala
@@ -1,31 +1,14 @@
 package de.choffmeister.asserthub.managers
 
 import org.squeryl.Query
+import org.squeryl.KeyedEntityDef
 
 import de.choffmeister.asserthub.models._
 import de.choffmeister.asserthub.models.Dsl._
 
 object UserManager extends EntityRepository[User] {
-  def all: List[User] = inTransaction(from(Database.users)(u => select(u) orderBy(u.id asc)).toList)
-
-  def find(id: Long): Option[User] = inTransaction(Database.users.lookup(id))
-
-  def insert(user: User): User = inTransaction(Database.users.insert(user))
-
-  def update(user: User): User = inTransaction {
-    Database.users.update(user)
-    user
-  }
-
-  def delete(id: Long): Option[User] = inTransaction {
-    find(id) match {
-      case Some(user) =>
-        Database.users.deleteWhere(u => u.id === user.id)
-        Some(user)
-      case _ =>
-        None
-    }
-  }
+  val keyDef = userKED
+  val table = Database.users
 
   /**
    * Creates a new user and persists it to the database.
--- src/main/scala/de/choffmeister/asserthub/models/Entity.scala
+++ src/main/scala/de/choffmeister/asserthub/models/Entity.scala
@@ -1,32 +1,50 @@
 package de.choffmeister.asserthub.models
 
+import org.squeryl.Table
+import org.squeryl.KeyedEntityDef
+import de.choffmeister.asserthub.models.Dsl._
+
 trait Entity {
   val id: Long
 }
 
 trait EntityRepository[T <: Entity] {
+  implicit val keyDef: KeyedEntityDef[T, Long]
+  val table: Table[T]
+
   /**
    * Returns a list of all available entities.
    */
-  def all: List[T]
+  def all: List[T] = inTransaction(from(table)(e => select(e) orderBy(e.id asc)).toList)
 
   /**
    * Returns a specific entity by it's ID.
    */
-  def find(id: Long): Option[T]
+  def find(id: Long): Option[T] = inTransaction(table.lookup(id))
 
   /**
    * Inserts a new entity.
    */
-  def insert(entity: T): T
+  def insert(entity: T): T = inTransaction(table.insert(entity))
 
   /**
    * Updates an entity.
    */
-  def update(entity: T): T
+  def update(entity: T): T = inTransaction {
+    table.update(entity)
+    entity
+  }
 
   /**
    * Deletes a specific entity by it's ID.
    */
-  def delete(id: Long): Option[T]
+  def delete(id: Long): Option[T] = inTransaction {
+    find(id) match {
+      case Some(entity) =>
+        table.deleteWhere(e => e.id === entity.id)
+        Some(entity)
+      case _ =>
+        None
+    }
+  }
 }
