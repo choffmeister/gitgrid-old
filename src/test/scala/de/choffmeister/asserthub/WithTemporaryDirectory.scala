f44b8e713b9859c7c592f6a89a6bde1a0fddb4f2
Christian Hoffmeister <mail@choffmeister.de>
2014-02-06 06:26:38 +0100

    Add specs2 scope WithTemporaryDirectory

---
+++ src/test/scala/de/choffmeister/asserthub/WithTemporaryDirectory.scala
@@ -0,0 +1,17 @@
+package de.choffmeister.asserthub
+
+import java.io.File
+import java.util.UUID
+import org.specs2.specification.Scope
+
+/**
+ * Test scope with a value directory of type java.io.File pointing
+ * to a new directory within the temp directory.
+ */
+class WithTemporaryDirectory(create: Boolean) extends Scope {
+  val directory = {
+    val dir = new File(System.getProperty("java.io.tmpdir"), UUID.randomUUID.toString)
+    if (create) dir.mkdirs()
+    dir
+  }
+}
