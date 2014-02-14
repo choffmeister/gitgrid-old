99e7bc09783798743166ac667211fa3128b56c5c
Christian Hoffmeister <mail@choffmeister.de>
2014-02-14 09:02:17 +0100

    Rename ApplicationActor -> HttpServiceActor

--- src/main/scala/com/gitgrid/Application.scala
+++ src/main/scala/com/gitgrid/Application.scala
@@ -2,33 +2,8 @@ package com.gitgrid
 
 import akka.actor._
 import akka.io.IO
-import com.gitgrid.git.GitHttpRequest
-import com.gitgrid.git.GitHttpServiceActor
-import com.gitgrid.webapi.WebApiServiceActor
 import scala.concurrent.duration._
 import spray.can.Http
-import spray.http._
-import spray.http.StatusCodes._
-
-class ApplicationActor extends Actor with ActorLogging {
-  implicit val executionContext = context.dispatcher
-  val webApiHttpService = context.actorOf(Props[WebApiServiceActor], "webapi-http-service")
-  val gitHttpService = context.actorOf(Props[GitHttpServiceActor], "git-http-service")
-
-  def receive = {
-    case akka.io.Tcp.Connected(_, _) =>
-      sender ! akka.io.Tcp.Register(self)
-    case req@HttpRequest(_, uri, _, _, _) if uri.path.startsWith(Uri.Path("/api")) =>
-      log.debug(req.toString())
-      webApiHttpService.tell(req, sender)
-    case req@GitHttpRequest(_, _, _, _) =>
-      log.debug(req.toString())
-      gitHttpService.tell(req, sender)
-    case o =>
-      log.debug("Unknown message received: " + o.toString())
-      sender ! HttpResponse(status = NotFound)
-  }
-}
 
 class Application extends Bootable {
   implicit val system = ActorSystem("gitgrid")
@@ -37,9 +12,9 @@ class Application extends Bootable {
     // generate test data
     TestDataGenerator.generate()
 
-    // start webservice
-    val httpService = system.actorOf(Props[ApplicationActor], "application")
-    IO(Http) ! Http.Bind(httpService, interface = Config.httpInterface, port = Config.httpPort)
+    val httpServiceActor = system.actorOf(Props[HttpServiceActor], "httpservice")
+
+    IO(Http) ! Http.Bind(httpServiceActor, interface = Config.httpInterface, port = Config.httpPort)
   }
 
   def shutdown = {
---
+++ src/main/scala/com/gitgrid/HttpServiceActor.scala
@@ -0,0 +1,29 @@
+package com.gitgrid
+
+import akka.actor._
+import akka.routing._
+import com.gitgrid.git._
+import com.gitgrid.webapi._
+import spray.http._
+import spray.http.StatusCodes._
+
+class HttpServiceActor extends Actor with ActorLogging {
+  implicit val executionContext = context.dispatcher
+
+  val webApiServiceActor = context.actorOf(Props[WebApiServiceActor].withRouter(FromConfig), "webapi")
+  val gitHttpServiceActor = context.actorOf(Props[GitHttpServiceActor].withRouter(FromConfig), "git")
+
+  def receive = {
+    case akka.io.Tcp.Connected(_, _) =>
+      sender ! akka.io.Tcp.Register(self)
+    case req@HttpRequest(_, uri, _, _, _) if uri.path.startsWith(Uri.Path("/api")) =>
+      log.debug(req.toString())
+      webApiServiceActor.tell(req, sender)
+    case req@GitHttpRequest(_, _, _, _) =>
+      log.debug(req.toString())
+      gitHttpServiceActor.tell(req, sender)
+    case o =>
+      log.debug("Unknown message received: " + o.toString())
+      sender ! HttpResponse(status = NotFound)
+  }
+}
